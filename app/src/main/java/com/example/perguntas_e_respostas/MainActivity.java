2ea7df51e65b2d21e4be05cb70aca95659a64751
Dato <datocarneiro1@outlook.com>
2025-05-14 01:11:58 -0300

    update frag

--- app/src/main/java/com/example/perguntas_e_respostas/MainActivity.java
+++ app/src/main/java/com/example/perguntas_e_respostas/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.perguntas_e_respostas;
 
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -15,10 +17,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.frameLayout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        JogarFragment fragment = new JogarFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment).commit();
+
+
     }
 }
+
+
